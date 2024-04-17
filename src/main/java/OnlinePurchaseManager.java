de9cc1f86e329d1bfad1f1d4516f31b61e9d854c
Sebastian Dowell <142616573+SebastianAPCS@users.noreply.github.com>
2024-04-17 09:45:57 -0700

    Update OnlinePurchaseManager.java

--- src/main/java/OnlinePurchaseManager.java
+++ src/main/java/OnlinePurchaseManager.java
@@ -24,7 +24,7 @@ public class OnlinePurchaseManager
     for (int i = 0; i < purchases.size() - 1; i++) {
       if (purchases.get(i).equals(purchases.get(i + 1))) return true;
     }
-    reeturn false;
+    return false;
   }
   public OnlinePurchaseManager()
   {
